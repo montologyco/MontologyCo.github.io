4950d0ab8b12a8022e01d09c2c77c90d3c6573d5
Montology <solutions@montologyco.com>
2024-11-25 18:53:13 -0500

    boiler links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import config from '../../company.json';
+import boilerplate from '../../boilerplate.json'; // Import the boilerplate JSON file
 
 function Footer() {
   const currentYear = new Date().getFullYear();
@@ -7,7 +8,17 @@ function Footer() {
   return (
     <>
       <footer>
-        {currentYear} &copy; {config.registeredName}. All rights reserved. {config.DBA} and the {config.DBA} logo are trademarks of {config.registeredName}
+        <div>
+          {currentYear} &copy; {config.registeredName}. All rights reserved. {config.DBA} and the {config.DBA} logo are trademarks of {config.registeredName}.
+        </div>
+        <div>
+          {boilerplate.links.map((link, index) => (
+            <span key={index}>
+              <a href={link.url}>{link.name}</a>
+              {index < boilerplate.links.length - 1 && ' | '}
+            </span>
+          ))}
+        </div>
       </footer>
     </>
   );
