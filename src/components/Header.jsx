00fe762606ee37f7fce15648160e6dbe750346bb
Montology <solutions@montologyco.com>
2024-11-25 19:28:01 -0500

    links added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import logo from '../assets/IconicLogo.png'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import the Link component
+import logo from '../assets/IconicLogo.png';
 
 function Header() {
-
   return (
     <>
       <header>
-        <a href="https://www.montologyco.com" target="_blank">
-          <img src={logo} className="logo" alt="logo"/>
-        </a>
+        <Link to="/"><img src={logo} alt="Montology Logo" /></Link>
         <nav>
-          <ul>test</ul>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/test1">Test 1</Link></li>
+            <li><Link to="/test2">Test 2</Link></li>
+          </ul>
         </nav>
       </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
