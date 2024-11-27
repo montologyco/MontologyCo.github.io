8b10d6073c48512423f97d8945dc70fe7e37397a
Montology <solutions@montologyco.com>
2024-11-27 14:43:02 -0500

    added background, removed header bg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import the Link component
+import { Link } from 'react-router-dom';
 import logo from '../assets/IconicLogo.png';
 
 function Header() {
@@ -8,11 +8,7 @@ function Header() {
       <header>
         <Link to="/"><img src={logo} alt="Montology Logo" /></Link>
         <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/test1">Test 1</Link></li>
-            <li><Link to="/test2">Test 2</Link></li>
-          </ul>
+          <Link to="/about">About</Link>
         </nav>
       </header>
     </>
