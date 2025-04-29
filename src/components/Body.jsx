e461e11e9d8e930e429e1891eef3f2514441c8fd
Montology <solutions@montologyco.com>
2025-04-29 14:27:40 -0400

    added aws- testing hello

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MontologyTagline from '../assets/MontologyTagline.jsx';
 
-function Body({loggedIn}) {
+function Body({ loggedIn }) {
+  useEffect(() => {
+    fetch("https://abc123xyz.execute-api.us-east-1.amazonaws.com/hello")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data); // Should log: { message: "Hi, I'm a server" }
+      })
+      .catch(err => console.error("API error:", err));
+  }, []);
+
   return (
     <>
       <div id="body">
