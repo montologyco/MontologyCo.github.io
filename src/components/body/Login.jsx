7fe1db2ed78902a9f1aa2910485ada042201af6b
Montology <solutions@montologyco.com>
2025-04-29 19:33:28 -0400

    login buttons

--- src/components/body/Login.jsx
+++ src/components/body/Login.jsx
@@ -7,25 +7,28 @@ function Login({ loggedIn }) {
   const [showInput, setShowInput] = useState(false);
 
   return (
-    <div>
-    {!loggedIn ? (
-      <div>
+    !loggedIn ? (
+      <>
         <div id="login">
           <h1>Log in:</h1>
           <p>Username: <input type="text" placeholder="Username" /></p>
           <p>Password: <input type="password" placeholder="Password" /></p>
         </div>
+        {showInput && (
+          <div>
+            <p>Button</p>
+          </div>
+        )}
         <div id="login-buttons">
           <p>Don't have an account? <Link to="/FAQ">FAQ</Link></p>
           <p>Need help? <Link to="/contact">Contact</Link></p>
         </div>
-      </div>
+      </>
     ) : (
       <div>
         <p>what</p>
       </div>
-    )}
-    </div>
+    )
   );
 }
 
