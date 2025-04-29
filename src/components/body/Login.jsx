2b09e955f23c87d2aa0f3a4d7d0b136ad72899e5
Montology <solutions@montologyco.com>
2025-04-29 19:41:04 -0400

    login buttons

--- src/components/body/Login.jsx
+++ src/components/body/Login.jsx
@@ -1,34 +1,49 @@
 // Login.jsx
 
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 function Login({ loggedIn }) {
-  const [showInput, setShowInput] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (loggedIn) return <Navigate to="/dashboard" />;
+
+  const isFilled = username !== '' && password !== '';
 
   return (
-    !loggedIn ? (
-      <>
-        <div id="login">
-          <h1>Log in:</h1>
-          <p>Username: <input type="text" placeholder="Username" /></p>
-          <p>Password: <input type="password" placeholder="Password" /></p>
-        </div>
-        {showInput && (
-          <div>
-            <p>Button</p>
-          </div>
-        )}
-        <div id="login-buttons">
-          <p>Don't have an account? <Link to="/FAQ">FAQ</Link></p>
-          <p>Need help? <Link to="/contact">Contact</Link></p>
-        </div>
-      </>
-    ) : (
-      <div>
-        <p>what</p>
+    <>
+      <div id="login">
+        <h1>Log in:</h1>
+        <p>
+          Username: 
+          <input 
+            type="text" 
+            placeholder="Username" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </p>
+        <p>
+          Password: 
+          <input 
+            type="password" 
+            placeholder="Password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </p>
       </div>
-    )
+
+      <div>
+        {isFilled && <button>Log In</button>}
+      </div>
+
+      <div id="login-FAQ">
+        <p>Don't have an account? <Link to="/FAQ">FAQ</Link></p>
+        <p>Need help? <Link to="/contact">Contact</Link></p>
+      </div>
+    </>
   );
 }
 
