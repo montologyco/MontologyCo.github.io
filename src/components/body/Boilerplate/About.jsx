670669d4784050f4fe68775a60500c4f91d49d02
Montology <solutions@montologyco.com>
2025-04-29 19:06:01 -0400

    404 handling

---
+++ src/components/body/Boilerplate/About.jsx
@@ -0,0 +1,43 @@
+// About.jsx
+
+import React from 'react';
+
+function About() {
+  return (
+    <div id="about">
+      <h1>About Us</h1>
+        <p>Montology is a platform dedicated to providing the best tools and resources for your needs.</p>
+        <p>Our mission is to empower users with the knowledge and tools they need to succeed.</p>
+        <p>We are committed to continuous improvement and innovation.</p>
+        <p>For more information, please visit our <a href="https://montologyco.com">website</a></p>
+        <p>Follow us on our social media channels for updates and news!</p>
+        <p>Twitter: <a href="https://twitter.com/montologyco">@montologyco</a></p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/company/montologyco">Montology</a></p>
+        <p>GitHub: <a href="https://github.com/montologyco"></a></p>
+        <p>Facebook: <a href="https://www.facebook.com/montologyco">Montology</a></p>
+        <p>Instagram: <a href="https://www.instagram.com/montologyco">Montology</a></p>
+        <p>Discord: <a href="https://discord.gg/montologyco">Montology</a></p>
+        <p>Reddit: <a href="https://www.reddit.com/r/montologyco">Montology</a></p>
+        <p>YouTube: <a href="https://www.youtube.com/c/montologyco">Montology</a></p>
+        <p>Medium: <a href="https://medium.com/@montologyco">Montology</a></p>
+        <p>GitLab: <a href="https://gitlab.com/montologyco">Montology</a></p>
+        <p>Stack Overflow: <a href="https://stackoverflow.com/users/montologyco">Montology</a></p>
+        <p>Vimeo: <a href="https://vimeo.com/montologyco">Montology</a></p>
+        <p>WhatsApp: <a href="https://wa.me/1234567890">Montology</a></p>
+        <p>We look forward to connecting with you!</p>
+        <p>Thank you for visiting Montology!</p>
+        <p>We hope you find our platform useful and informative.</p>
+        <p>If you have any questions or feedback, please feel free to reach out to us.</p>
+        <p>We are always here to help!</p>
+        <p>Stay tuned for more updates and features coming soon!</p>
+        <p>Best regards,</p>
+        <p>The Montology Team</p>
+        <p>Montology Inc.</p>
+        <p>123 Montology St.</p>
+        <p>Montology City, MT 12345</p>
+        <p>Phone: (123) 456-7890</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/body/Boilerplate/Contact.jsx
@@ -0,0 +1,15 @@
+// Contact.jsx
+
+import React from 'react';
+
+function Contact({ loggedIn }) {
+  return (
+    <div id="contact">
+        <h1>Contact Us</h1>
+        <p>If you have any questions or feedback, please reach out to us!</p>
+        <p>Email: <a href="mailto:admin@montologyco.com" /></p>
+    </div>
+  );
+}
+
+export default Contact;
