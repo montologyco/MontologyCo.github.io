da74336fe7be2ec94e487dae7e8335e05239cec6
Montology <solutions@montologyco.com>
2025-04-29 19:11:43 -0400

    login

--- src/components/body/Login.jsx
+++ src/components/body/Login.jsx
@@ -8,28 +8,21 @@ function Login({ loggedIn }) {
 
   return (
     <div id="login">
-      <h1>Welcome to Montology!</h1>
-      <p>Montology is a platform for sharing and discovering knowledge.</p>
-      <p>To get started, please log in or create an account.</p>
-      <p>Already have an account? <Link to="/login">Log In</Link></p>
-      <p>Don't have an account? <Link to="/register">Register</Link></p>
-      <p>Need help? <Link to="/help">Help</Link></p>
-
-      {/* {!loggedIn ? (
+      {!loggedIn ? (
         <div>
-          <Link onClick={() => setShowInput(!showInput)}>Log In</Link>
-          {showInput && (
-            <div>
-              <p>Username: <input type="text" placeholder="Username" /></p>
-              <p>Password: <input type="password" placeholder="Password" /></p>
-            </div>
-          )}
+          <h1>Log in:</h1>
+          <p>Username: <input type="text" placeholder="Username" /></p>
+          <p>Password: <input type="password" placeholder="Password" /></p>
+          <p>To get started, please log in or create an account.</p>
+          <p>Already have an account? <Link to="/login">Log In</Link></p>
+          <p>Don't have an account? <Link to="/register">Register</Link></p>
+          <p>Need help? <Link to="/help">Help</Link></p>
         </div>
       ) : (
         <div>
-          <Link>Log Out</Link>
+          <p>what</p>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
