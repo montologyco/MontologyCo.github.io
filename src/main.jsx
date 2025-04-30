8e04b6cee1c6c2e5d79986354e64ab152d21f640
Montology <solutions@montologyco.com>
2025-04-30 08:25:04 -0400

    config edits

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,18 @@
 // main.jsx
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Amplify } from 'aws-amplify';
-import awsconfig from '../aws-exports.js';
+import awsconfig from './assets/aws-exports';
 
-function Main() {
-  const [isConfigured, setIsConfigured] = useState(false);
-
-  useEffect(() => {
-    Amplify.configure(awsconfig);
-    setIsConfigured(true);
-    console.log("Amplify configured successfully!");
-  }, []);
-
-  if (!isConfigured) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <App />
-  );
-}
+Amplify.configure(awsconfig);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>
 );
