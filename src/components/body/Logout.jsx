c65d0528089c563c52cafff504ec7241921b6c4d
Montology <solutions@montologyco.com>
2025-04-30 13:13:53 -0400

    Log Out

--- src/components/body/Logout.jsx
+++ src/components/body/Logout.jsx
@@ -1,12 +1,16 @@
 // Logout.jsx
 
-import React from 'react';
+import { useEffect } from 'react';
 import { signOut } from '@aws-amplify/auth';
 
-function Logout() {
-  signOut();
-  console.log("potato");
-  return null;
+function Logout({ setIsAuthenticated }) {
+  useEffect(() => {
+    signOut();
+    setIsAuthenticated(false);
+    console.log("User has been signed out.");
+  }, []);
+
+  return null; // No UI, just perform the action
 }
 
 export default Logout;
