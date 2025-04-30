c36515c57b48f827b218d23f3d643eb8811a914b
Montology <solutions@montologyco.com>
2025-04-30 08:03:55 -0400

    config edits

--- aws-exports.js
+++ aws-exports.js
@@ -4,7 +4,7 @@ export default {
   signIn: {
     region: 'us-east-1',
     userPoolId: 'us-east-1_vwGK8wsAx',
-    userPoolWebClientId: 'pmbfufdc2c5qnf8qjhaj58p7u',
+    userPoolWebClientId: "pmbfufdc2c5qnf8qjhaj58p7u",
     mandatorySignIn: true,
     authenticationFlowType: 'USER_PASSWORD_AUTH',
   },
