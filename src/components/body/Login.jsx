0af6a65a123176faca017de1bcb085c56c48493e
Montology <solutions@montologyco.com>
2025-04-30 11:10:53 -0400

    redirect?

--- src/components/body/Login.jsx
+++ src/components/body/Login.jsx
@@ -1,36 +1,30 @@
 // Login.jsx
 
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, redirect } from 'react-router-dom';
 import { signIn } from '@aws-amplify/auth';
 
-function Login( {isAuthenticated}) {
+function Login({ setIsAuthenticated }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [isAuthenticated, setisAuthenticated] = useState(false);
-
-  if (isAuthenticated) return <Navigate to="/dashboard" />;
-
+  
   const handleLogin = async (e) => {
     e.preventDefault();
     setLoading(true);
 
     try {
-      const user = await signIn({
-        username,
-        password,
-      });
-
-      // You can handle different next steps based on MFA or other challenges here if needed.
+      const user = await signIn({ username, password });
       console.log('Login successful:', user);
-      setisAuthenticated(true);
+      navigate to /Dashboard;
+      setIsAuthenticated(true);
     } catch (err) {
       setError(err.message);
       console.error('Login error:', err);
     } finally {
       setLoading(false);
+      redirect('/dashboard'); // Redirect to the home page after login
     }
   };
 
