8279c020e8ec5bcd8126e5ea6b5267988b2b4731
Montology <solutions@montologyco.com>
2025-04-30 07:15:42 -0400

    Node Module

---
+++ aws-exports.js
@@ -0,0 +1,11 @@
+// aws-exports.js
+
+export default {
+    Auth: {
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_vwGK8wsAx',
+      userPoolWebClientId: 'pmbfufdc2c5qnf8qjhaj58p7u',
+      mandatorySignIn: true,
+      authenticationFlowType: 'USER_PASSWORD_AUTH',
+    },
+  };  
--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Amplify } from 'aws-amplify';
+import awsconfig from '../aws-exports'; // Make sure the path is correct
 
-// AWS Amplify Configuration
-Amplify.configure({
-  Auth: {
-    region: 'us-east-1', // Your region
-    userPoolId: 'us-east-1_vwGK8wsAx', // Your Cognito User Pool ID
-    userPoolWebClientId: 'pmbfufdc2c5qnf8qjhaj58p7u', // Your Cognito App Client ID (found in your Cognito app client settings)
-    mandatorySignIn: true, // If you want to enforce sign-in for your app
-    authenticationFlowType: 'USER_PASSWORD_AUTH', // User authentication type (you can adjust this based on your use case)
-  },
-});
+Amplify.configure(awsconfig); // Applying the AWS Amplify configuration
 
 const container = document.getElementById('root');
 const root = createRoot(container);
