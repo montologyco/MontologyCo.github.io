7181e63cc9578c324f29d3a0c68a6ac5c243880c
Montology <solutions@montologyco.com>
2025-04-30 11:49:46 -0400

    redirect not working

--- src/components/body/Login.jsx
+++ src/components/body/Login.jsx
@@ -1,30 +1,22 @@
 // Login.jsx
 
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
 import { signIn } from '@aws-amplify/auth';
 
 function Login({ setIsAuthenticated }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [redirect, setRedirect] = useState(false); // Track redirect state
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    setLoading(true);
 
     try {
-      setLoading(true);
-      setRedirect(false);
       const user = await signIn({ username, password });
     } catch (err) {
       setError(err.message);
       console.error('Login error:', err);
     } finally {
-      setLoading(false);
-      setRedirect(true);
       setIsAuthenticated(true);
     }
   };
@@ -51,8 +43,8 @@ function Login({ setIsAuthenticated }) {
             required
           />
         </p>
-        <button type="submit" disabled={!username || !password || loading}>
-          {loading ? 'Logging In...' : 'Log In'}
+        <button type="submit" disabled={!username || !password}>
+          {'Log In'}
         </button>
       </form>
       {error && <p style={{ color: 'red' }}>{error}</p>}
