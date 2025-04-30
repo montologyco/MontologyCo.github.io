e455bcffa574e239879adc72579c6ddc7d92774b
Montology <solutions@montologyco.com>
2025-04-30 09:58:33 -0400

    Auth Checker- tokens?

--- src/AWS/aws-authChecker.jsx
+++ src/AWS/aws-authChecker.jsx
@@ -5,31 +5,25 @@ import { fetchAuthSession } from '@aws-amplify/core';
 
 const checkSession = async () => {
   try {
-    const session = await fetchAuthSession();  // Fetch the session info
+    const session = await fetchAuthSession();
+    console.log("Session info:", session);
 
-    console.log("Session info:", session); // Log the session info for debugging
-
-    // Check if session contains a valid idToken (indicating authentication)
-    if (session && session.idToken) {
+    if (session && session.tokens && session.tokens.idToken) {
       console.log("User is authenticated");
-      // Proceed with the authenticated flow
     } else {
       console.log("No valid session found");
-      // Redirect or prompt user to log in
     }
   } catch (error) {
     console.error("Error checking session:", error);
-    // Handle errors (e.g., session expired or not found)
   }
 };
 
 const AuthChecker = ({ children }) => {
-  // Call checkSession when the component mounts
   useEffect(() => {
     checkSession();
   }, []);
 
-  return children; // Render the children if authenticated
+  return children;
 };
 
 export default AuthChecker;
