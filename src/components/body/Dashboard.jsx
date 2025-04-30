a3a7486622d2b0e04269a151c3858cab08cb81dd
Montology <solutions@montologyco.com>
2025-04-30 12:35:40 -0400

    AuthChecker

--- src/components/body/Dashboard.jsx
+++ src/components/body/Dashboard.jsx
@@ -1,12 +1,14 @@
 // Dashboard.jsx
 
 import React from 'react';
+import AuthChecker from '../../AWS/aws-authChecker.jsx';
 
 function Dashboard({ isAuthenticated, setIsAuthenticated}) {
   return (
     <div id="Dashboard">
-        <h1>Dashboard</h1>
-        <p>what the fuck</p>
+      <AuthChecker setAuthState={setIsAuthenticated} />
+      <h1>Dashboard</h1>
+      <p>what the fuck</p>
     </div>
   );
 }
