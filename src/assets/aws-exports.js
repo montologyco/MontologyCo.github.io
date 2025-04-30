fb559a1e71d292b36fb04240d736293593482716
Montology <solutions@montologyco.com>
2025-04-30 08:30:53 -0400

    config edits

--- src/assets/aws-exports.js
+++ src/assets/aws-exports.js
@@ -1,12 +1,10 @@
 // aws-exports.js
 
 export default {
-  Auth: {
-    Cognito: {
-      userPoolId: 'us-east-1_vwGK8wsAx',
-      userPoolWebClientId: 'pmbfufdc2c5qnf8qjhaj58p7u',
-      mandatorySignIn: true,
-      authenticationFlowType: 'USER_PASSWORD_AUTH',
-    }
+  signIn: {
+    userPoolId: 'us-east-1_vwGK8wsAx',
+    userPoolWebClientId: 'pmbfufdc2c5qnf8qjhaj58p7u',
+    mandatorySignIn: true,
+    authenticationFlowType: 'USER_PASSWORD_AUTH'
   },
 };
