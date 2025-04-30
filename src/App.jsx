3f59760fa89bc09690060b9fb9422562d3791760
Montology <solutions@montologyco.com>
2025-04-30 10:10:50 -0400

    Auth Checker- tokens?

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import './styles/index.scss';
 import Header from './components/Header.jsx';
 import Body from './components/Body.jsx';
 import Footer from './components/Footer.jsx';
+import AuthChecker from './AWS/aws-authChecker';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <Router>
+
+          <AuthChecker>
       <Header
         loggedIn={loggedIn}
       />
@@ -19,6 +22,8 @@ function App() {
         loggedIn={loggedIn}
       />
       <Footer />
+
+          </AuthChecker>
     </Router>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { Amplify } from 'aws-amplify';
 import AWSconfig from './AWS/aws-config';
-import AuthChecker from './AWS/aws-authChecker';
 
 Amplify.configure(AWSconfig);
 
@@ -14,8 +13,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <AuthChecker>
       <App />
-    </AuthChecker>
   </React.StrictMode>
 );
