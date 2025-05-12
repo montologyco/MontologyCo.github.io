dae6a693c5ae165545c9d6383f48b68f641461b6
Montology <solutions@montologyco.com>
2025-05-12 15:44:41 -0400

    DynamoDB

--- src/shells/TableShell/TableShell-Directory.jsx
+++ src/shells/TableShell/TableShell-Directory.jsx
@@ -1,9 +1,6 @@
 // TableShell-Directory.jsx
 
 import React from 'react';
-import Search from './Search/Search.jsx';
-import TableShellDirectory from './TableShell/TableShell-Directory.jsx';
-import TableShellProfile from './TableShell/TableShell-Profile.jsx';
 
 const TableShellDirectory = () => {
   return (
