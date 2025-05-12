866743167440aa10ba620e35bc2899a92720093c
Montology <solutions@montologyco.com>
2025-05-12 16:58:09 -0400

    TableShell

--- src/shells/TableShell/TableShell-Divider.jsx
+++ src/shells/TableShell/TableShell-Divider.jsx
@@ -3,8 +3,7 @@
 import React, { useRef, useEffect } from 'react';
 
 const TableShellDivider = ({ onDrag }) => {
-  const isDragging = useRef(false); // Reference to track if the drag is happening
-  const dividerRef = useRef(null); // Reference for the divider element
+  const isDragging = useRef(false);
 
   useEffect(() => {
     const handleMouseMove = (e) => {
@@ -12,32 +11,23 @@ const TableShellDivider = ({ onDrag }) => {
         onDrag(e.movementX);
       }
     };
-
     const handleMouseUp = () => {
       isDragging.current = false;
     };
 
-    // Adding event listeners after component mounts
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('mouseup', handleMouseUp);
 
-    // Cleanup event listeners when component unmounts or updates
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
       window.removeEventListener('mouseup', handleMouseUp);
     };
   }, [onDrag]);
 
-  // Handle mouse down on the divider to start dragging
-  const handleMouseDown = (e) => {
-    isDragging.current = true;
-  };
-
   return (
     <div
-      ref={dividerRef} // Assigning the dividerRef to the divider div
       className="tableShell-divider"
-      onMouseDown={handleMouseDown}
+      onMouseDown={() => { isDragging.current = true; }}
     />
   );
 };
