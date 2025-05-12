8bd2d1cf448f69c0c25785ddb49c25d940f93750
Montology <solutions@montologyco.com>
2025-05-11 22:15:50 -0400

    DynamoDB

--- src/server/aws-sdk/aws-sdk-config.js
+++ src/server/aws-sdk/aws-sdk-config.js
@@ -1,16 +1,34 @@
 // aws-sdk-config.js
 
 import AWS from 'aws-sdk';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
-// Configure AWS SDK directly with Cognito Identity Pool
-AWS.config.update({
-  region: 'us-east-1',
-  credentials: new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:892afd16-442d-4b97-b627-264de66253e9',
-  }),
-});
+const getDynamoDB = async () => {
+  const session = await fetchAuthSession();
 
-// Create and export the DocumentClient instance
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+  const idToken = session.tokens?.idToken?.toString();
+  if (!idToken) {
+    throw new Error('No valid ID token found');
+  }
 
-export default dynamoDB;
+  AWS.config.update({
+    region: 'us-east-1',
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'us-east-1:892afd16-442d-4b97-b627-264de66253e9',
+      Logins: {
+        'cognito-idp.us-east-1.amazonaws.com/us-east-1_vwGK8wsAx': idToken,
+      },
+    }),
+  });
+
+  await new Promise((resolve, reject) => {
+    AWS.config.credentials.refresh((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+
+  return new AWS.DynamoDB.DocumentClient();
+};
+
+export default getDynamoDB;
