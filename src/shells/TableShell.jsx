c8bcb075637c8748135c81d997b8b9b3d7ff9650
Montology <solutions@montologyco.com>
2025-05-12 15:40:51 -0400

    DynamoDB

--- src/shells/TableShell.jsx
+++ src/shells/TableShell.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Search from './Search/Search.jsx';
 import TableShellDirectory from './TableShell/TableShell-Directory.jsx';
+import TableShellProfile from './TableShell/TableShell-Profile.jsx';
 
 const TableShell = () => {
   return (
@@ -17,6 +18,7 @@ const TableShell = () => {
         </div>
         <div className="tableShell-profile">
           <p>right/profile</p>
+          <TableShellProfile />
         </div>
       </div>
     </div>
