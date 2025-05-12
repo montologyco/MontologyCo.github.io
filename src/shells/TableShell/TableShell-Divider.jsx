db87b7966aa340cb1486019d9679120095c495fc
Montology <solutions@montologyco.com>
2025-05-12 16:50:36 -0400

    TableShell

--- src/shells/TableShell/TableShell-Divider.jsx
+++ src/shells/TableShell/TableShell-Divider.jsx
@@ -2,11 +2,31 @@
 
 import React from 'react';
 
-const TableShellDivider = () => {
+const TableShellDivider = ({ onDrag }) => {
+  const isDragging = useRef(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (isDragging.current) {
+        onDrag(e.movementX);
+      }
+    };
+    const handleMouseUp = () => {
+      isDragging.current = false;
+    };
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, []);
+
   return (
-    <div className="tableShell-divider">
-    </div>
+    <div
+      className="tableShell-divider"
+      onMouseDown={() => { isDragging.current = true; }}
+    />
   );
 };
-
 export default TableShellDivider;
--- src/shells/TableShell/TableShell-Panes.jsx
+++ src/shells/TableShell/TableShell-Panes.jsx
@@ -1,17 +1,23 @@
 // TableShell-Panes.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import TableShellDirectory from './TableShell-Directory.jsx';
 import TableShellProfile from './TableShell-Profile.jsx';
 import TableShellDivider from './TableShell-Divider.jsx';
 
 const TableShellPanes = () => {
+  const [directoryWidth, setDirectoryWidth] = useState(300); // initial width in px
+
   return (
-  <div className="tableShell-panes">
-    <TableShellDirectory />
-    <TableShellDivider />
-    <TableShellProfile />
-  </div>
+    <div className="tableShell-panes">
+      <div className="tableShell-directory" style={{ width: `${directoryWidth}px` }}>
+        <TableShellDirectory />
+      </div>
+      <TableShellDivider onDrag={delta => setDirectoryWidth(prev => Math.max(200, prev + delta))} />
+      <div className="tableShell-profile">
+        <TableShellProfile />
+      </div>
+    </div>
   );
 };
 
