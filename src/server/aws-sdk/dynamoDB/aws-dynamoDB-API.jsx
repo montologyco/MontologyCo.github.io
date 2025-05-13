a405117a4bf927c95b7df0a02c6551b79522efee
Montology <solutions@montologyco.com>
2025-05-12 20:54:57 -0400

    TableShell-Directory

--- src/server/aws-sdk/dynamoDB/aws-dynamoDB-API.jsx
+++ src/server/aws-sdk/dynamoDB/aws-dynamoDB-API.jsx
@@ -14,12 +14,20 @@ export const putParams = (item) => ({
 
 export const queryParams = (PK, SK) => ({
   TableName: TABLE_NAME,
-  KeyConditionExpression: 'begins_with(PK, :pk) AND begins_with(SK, :sk)',
+  KeyConditionExpression: 'PK = :pk',
   ExpressionAttributeValues: {
     ':pk': PK,
-    ':sk': SK,
   },
 });
+
+// export const queryParams = (PK, SK) => ({
+//   TableName: TABLE_NAME,
+//   KeyConditionExpression: 'begins_with(PK, :pk) AND begins_with(SK, :sk)',
+//   ExpressionAttributeValues: {
+//     ':pk': PK,
+//     ':sk': SK,
+//   },
+// });
 
 export const updateParams = (PK, SK, updateExpression, expressionAttributeValues) => ({
   TableName: TABLE_NAME,
