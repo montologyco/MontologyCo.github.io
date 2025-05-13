6717a09f449234373393ba6d335a8b5144583959
Montology <solutions@montologyco.com>
2025-05-12 20:43:08 -0400

    TableShell-Directory

--- src/server/aws-sdk/dynamoDB/services/aws-dynamoDB-queryItems-API.jsx
+++ src/server/aws-sdk/dynamoDB/services/aws-dynamoDB-queryItems-API.jsx
@@ -7,7 +7,7 @@ const queryItems = async (PK, SK, query) => {
   const DynamoDB = await getDynamoDB();
   const params = queryParams(PK, SK);
   try {
-    console.log("PK, SK:", PK, SK, query); //query = placeholder
+    console.log("PK, SK:", PK, typeof PK, SK, typeof SK, query); //query = placeholder
     const result = await DynamoDB.query(params).promise();
     console.log("Query result:", result);
     return result.Items;
