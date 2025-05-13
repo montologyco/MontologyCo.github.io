0879b7a316b2e345c8069751e6491715f4df1395
Montology <solutions@montologyco.com>
2025-05-12 21:46:26 -0400

    TableShell-Directory

--- src/shells/TableShell/TableShell.jsx
+++ src/shells/TableShell/TableShell.jsx
@@ -8,7 +8,7 @@ import TableShellFilter from './TableShell-Filter.jsx';
 const TableShell = ({ name }) => {
   const [directory, setDirectory] = useState([]);
   const [inputValue, setInputValue] = useState('');
-  const SK = 'individual';
+  const SK = '';
 
   const handleSearch = (searchQuery) => {
     setInputValue(searchQuery);
