9d0bf84d555378956e2df3dcdadf1d4670afc685
Montology <solutions@montologyco.com>
2025-05-12 21:40:00 -0400

    TableShell-Directory

--- src/shells/TableShell/TableShell-Filter.jsx
+++ src/shells/TableShell/TableShell-Filter.jsx
@@ -5,7 +5,7 @@ import queryItems from '../../server/aws-sdk/dynamoDB/services/aws-dynamoDB-quer
 import applications from '../../navigation/applications.json';
 
 const TableShellFilter = ({ name, SK, inputValue, setDirectory }) => {
-    const { PK, SK } = applications.links.find(link => link.name === name) || {};
+    const { PK } = applications.links.find(link => link.name === name) || {};
     useEffect(() => {
         const fetchData = async () => {
             const data = await queryItems(PK, SK, inputValue)
