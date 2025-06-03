9c7e67472b19a4238d6cf55e0585cc4dad1764b7
Montology <solutions@montologyco.com>
2025-06-03 14:55:23 -0400

    TableProfile

--- src/shells/ProfileShell/ProfileShell.jsx
+++ src/shells/ProfileShell/ProfileShell.jsx
@@ -1,7 +1,7 @@
 // ProfileShell.jsx
 
 import React, { useEffect, useState } from 'react';
-import AuthChecker from '../../../server/amplify/aws-amplify-authChecker-API.jsx';
+import AuthChecker from '../../server/amplify/aws-amplify-authChecker-API.jsx';
 import getItem from '../../../server/aws-sdk/dynamoDB/services/aws-dynamoDB-getItem-API.jsx';
 
 function ProfileShell({ directoryitem, SKs = [], setIsAuthenticated = () => {} }) {
