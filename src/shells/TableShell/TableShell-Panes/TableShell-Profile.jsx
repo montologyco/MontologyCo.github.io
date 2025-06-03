f1355fb1ee3392e5ab25e4cc3846e6f81a9be6e0
Montology <solutions@montologyco.com>
2025-06-03 15:02:47 -0400

    TableProfile

--- src/shells/TableShell/TableShell-Panes/TableShell-Profile.jsx
+++ src/shells/TableShell/TableShell-Panes/TableShell-Profile.jsx
@@ -1,7 +1,7 @@
 // TableShell-Profile.jsx
 
 import React from 'react';
-import ProfileShell from './ProfileShell.jsx';
+import ProfileShell from '../../ProfileShell/ProfileShell.jsx';
 
 function TableShellProfile({ directoryitem, SKs = [], setIsAuthenticated = () => {} }) {
   return (
