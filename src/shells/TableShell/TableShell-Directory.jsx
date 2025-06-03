9fcf9bbc975b8204a693e7255a25d41b30e3911a
Montology <solutions@montologyco.com>
2025-06-03 10:37:30 -0400

    Table Query

--- src/shells/TableShell/TableShell-Directory.jsx
+++ src/shells/TableShell/TableShell-Directory.jsx
@@ -8,23 +8,23 @@ const TableShellDirectory = ({ directory, directoryWidth }) => {
     };
 
     const stringifyItem = (item) => {
-        const filteredItem = Object.entries(item)
-            .filter(([key]) => key !== 'PK' && key !== 'SK')
-            .map(([key, value]) => value)
-            .filter(Boolean);
-            
-        return filteredItem.join(' ');
+    const filteredItem = Object.entries(item)
+        .filter(([key]) => key !== 'PK' && key !== 'SK')
+        .map(([key, value]) => value)
+        .filter(Boolean);
+
+    return filteredItem.join(' ');
     };
 
     return (
         <div className="tableShell-directory" style={{ width: `${directoryWidth}px` }}>
             <ul>
                 {directory.map((directoryitem) => (
-                    <li key={directoryitem.PK}>
-                        <a href="#" onClick={() => handleItemClick(directoryitem)}>
-                            {stringifyItem(directoryitem)}
-                        </a>
-                    </li>
+                <li key={directoryitem.PK}>
+                    <a href="#" onClick={() => handleItemClick(directoryitem)}>
+                    {stringifyItem(directoryitem)}
+                    </a>
+                </li>
                 ))}
             </ul>
         </div>
