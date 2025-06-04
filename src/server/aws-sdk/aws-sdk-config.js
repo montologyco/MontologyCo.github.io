4afc14f2332c49ada1043e03c60fd600b0ef9165
Montology <solutions@montologyco.com>
2025-06-03 21:49:47 -0400

    Profile Sections

--- src/server/aws-sdk/aws-sdk-config.js
+++ src/server/aws-sdk/aws-sdk-config.js
@@ -4,36 +4,31 @@ import AWS from 'aws-sdk';
 import { fetchAuthSession } from 'aws-amplify/auth';
 
 const getDynamoDB = async () => {
-  try {
-    const session = await fetchAuthSession();
+  const session = await fetchAuthSession();
 
-    const idToken = session.tokens?.idToken?.toString();
-    if (!idToken) {
-      throw new Error('No valid ID token found');
-    }
-
-    AWS.config.update({
-      region: 'us-east-1',
-      credentials: new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'us-east-1:3fea43bf-0e53-454e-b4a4-d57c51599826',
-        Logins: {
-          'cognito-idp.us-east-1.amazonaws.com/us-east-1_vwGK8wsAx': idToken,
-        },
-      }),
-    });
-
-    await new Promise((resolve, reject) => {
-      AWS.config.credentials.refresh((err) => {
-        if (err) reject(new Error('Failed to refresh credentials: ' + err));
-        else resolve();
-      });
-    });
-
-    return new AWS.DynamoDB.DocumentClient();
-  } catch (error) {
-    console.error('Error setting up DynamoDB client:', error);
-    throw error;
+  const idToken = session.tokens?.idToken?.toString();
+  if (!idToken) {
+    throw new Error('No valid ID token found');
   }
+
+  AWS.config.update({
+    region: 'us-east-1',
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'us-east-1:3fea43bf-0e53-454e-b4a4-d57c51599826',
+      Logins: {
+        'cognito-idp.us-east-1.amazonaws.com/us-east-1_vwGK8wsAx': idToken,
+      },
+    }),
+  });
+
+  await new Promise((resolve, reject) => {
+    AWS.config.credentials.refresh((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+
+  return new AWS.DynamoDB.DocumentClient();
 };
 
 export default getDynamoDB;
