d1bf4f79c61d6864306ef9e07bf1547f6aa12a7d
Montology <solutions@montologyco.com>
2025-06-05 18:13:22 -0400

    Profile Sections

--- src/shells/ProfileShell/Sections/ProfileShell-Addresses.jsx
+++ src/shells/ProfileShell/Sections/ProfileShell-Addresses.jsx
@@ -2,6 +2,7 @@
 
 const ProfileShellAddresses = ({ item }) => {
   const addressSKs = Array.isArray(item.addresses) ? item.addresses : [];
+  console.log('ProfileShellAddresses', addressSKs);
 
   return (
     <div className="profile-addresses">
