a6aa65d899214708e2ceef3f31fb27986bdb9dd7
Montology <solutions@montologyco.com>
2025-06-11 11:36:37 -0400

    Profile Addresses

--- src/shells/ProfileShell/Sections/ProfileShell-Phones.jsx
+++ src/shells/ProfileShell/Sections/ProfileShell-Phones.jsx
@@ -4,15 +4,16 @@ const ProfileShellPhones = ({ item }) => {
   const phoneSet = item.phones;
   const phoneSKs = Array.isArray(phoneSet?.values) ? phoneSet.values : [];
 
-  console.log('ProfileShellPhones', phoneSKs);
-  console.log('ProfileShellPhones item', item);
-
   return (
     <div className="profile-phones">
       <h3>Phones</h3>
       {phoneSKs.map(phone => (
         <div key={phone}>
-          <p>{phone}</p>
+          <p>
+            {phone.length === 10
+              ? `(${phone.slice(0, 3)}) ${phone.slice(3, 6)}-${phone.slice(6)}`
+              : phone}
+          </p>
         </div>
       ))}
       {phoneSKs.length === 0 && <p>No phones linked.</p>}
