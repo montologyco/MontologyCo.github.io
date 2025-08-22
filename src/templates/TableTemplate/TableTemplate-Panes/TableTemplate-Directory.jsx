ce31c7e7b8d6ccb09057346ce6aa1ad0a8211369
Montology <solutions@montologyco.com>
2025-08-22 16:25:00 -0400

    Directory List --> table

--- src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
+++ src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
@@ -7,25 +7,13 @@ const TableTemplateDirectory = ({ directory, directoryWidth, onSelectItem, SKs =
     }
   };
 
-  const stringifyItem = (item) => {
-    const skPrefix = item.SK.match(/^[a-zA-Z]+/)?.[0]; // Extract type from SK like "individual0001" → "individual"
-    const skConfig = SKs.find(sk => sk.SK === skPrefix);
-    const SKheading = skConfig?.SKheading || [];
-
-    const values = SKheading
-      .map(SKheading => item[SKheading])
-      .filter(Boolean);
-
-    return values.join(' ');
-  };
-
   return (
     <div className="tableTemplate-directory" style={{ width: `${directoryWidth}px` }}>
       <ul>
         {directory.map((directoryitem) => (
           <li key={directoryitem.SK}>
             <a href="#" onClick={(e) => { e.preventDefault(); handleItemClick(directoryitem); }}>
-              {stringifyItem(directoryitem)}
+              {directoryitem}
             </a>
           </li>
         ))}
