bbf2fad69f17a82a6c3984822ed48318d8e58445
Montology <solutions@montologyco.com>
2025-08-22 16:32:33 -0400

    Directory table

--- src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
+++ src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
@@ -1,28 +1,35 @@
 // TableTemplate-Directory.jsx
 
-const TableTemplateDirectory = ({ directory, directoryWidth, onSelectItem }) => {
+const TableTemplateDirectory = ({ directory, directoryWidth, onSelectItem, SKs = [] }) => {
   const handleItemClick = (directoryitem) => {
     if (onSelectItem) {
       onSelectItem({ PK: directoryitem.PK, SK: directoryitem.SK });
     }
   };
 
+  const stringifyItem = (item) => {
+    const skPrefix = item.SK.match(/^[a-zA-Z]+/)?.[0]; // Extract type from SK like "individual0001" → "individual"
+    const skConfig = SKs.find(sk => sk.SK === skPrefix);
+    const SKheading = skConfig?.SKheading || [];
+
+    const values = SKheading
+      .map(SKheading => item[SKheading])
+      .filter(Boolean);
+
+    return values.join(' ');
+  };
+
   return (
-    <div
-      className="tableTemplate-directory"
-      style={{ width: `${directoryWidth}px`, overflowX: 'auto' }}
-    >
-      <table>
-        <tbody>
-          {directory.map((item, index) => (
-            <tr key={item.SK || index} onClick={() => handleItemClick(item)} style={{ cursor: 'pointer' }}>
-              {Object.values(item).map((value, i) => (
-                <td key={i}>{value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="tableTemplate-directory" style={{ width: `${directoryWidth}px` }}>
+      <ul>
+        {directory.map((directoryitem) => (
+          <li key={directoryitem.SK}>
+            <a href="#" onClick={(e) => { e.preventDefault(); handleItemClick(directoryitem); }}>
+              {stringifyItem(directoryitem)}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
