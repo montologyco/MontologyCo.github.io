f104295e3d7e8ba122386609becc06d6c08a5e2a
Montology <solutions@montologyco.com>
2025-08-22 18:13:00 -0400

    Upload Template

--- src/applications/Contacts/Contacts.jsx
+++ src/applications/Contacts/Contacts.jsx
@@ -1,16 +1,28 @@
 // Contacts.jsx
 
+import { useState } from 'react';
 import AuthChecker from '../../server/amplify/aws-amplify-authChecker-API.jsx';
 import TableTemplate from '../../templates/TableTemplate/TableTemplate.jsx';
 import UploadTemplate from '../../templates/UploadTemplate/UploadTemplate.jsx';
 
 function Contacts({ setIsAuthenticated }) {
+  const [showUpload, setShowUpload] = useState(false);
+
   return (
     <div>
       <AuthChecker setAuthState={setIsAuthenticated} />
-      <h1>Contacts <button>On Click, load UploadTemplate, else load TableTemplate</button> </h1> 
-      {/* <UploadTemplate setIsAuthenticated={setIsAuthenticated} application="Contacts" /> */}
-      <TableTemplate setIsAuthenticated={setIsAuthenticated} application="Contacts" />
+      <h1>
+        Contacts{' '}
+        <button onClick={() => setShowUpload(prev => !prev)}>
+          {showUpload ? 'View Table' : 'Upload Data'}
+        </button>
+      </h1>
+
+      {showUpload ? (
+        <UploadTemplate setIsAuthenticated={setIsAuthenticated} application="Contacts" />
+      ) : (
+        <TableTemplate setIsAuthenticated={setIsAuthenticated} application="Contacts" />
+      )}
     </div>
   );
 }
