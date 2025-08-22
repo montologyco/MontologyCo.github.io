ee22368a3c9923ab4163745edd578eae56776e35
Montology <solutions@montologyco.com>
2025-08-22 16:47:34 -0400

    Directory table

--- src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
+++ src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
@@ -1,5 +1,7 @@
 // TableTemplate-Directory.jsx
 
+import React from 'react';
+
 const TableTemplateDirectory = ({ directory, directoryWidth, onSelectItem, SKs = [] }) => {
   const handleItemClick = (directoryitem) => {
     if (onSelectItem) {
@@ -7,39 +9,18 @@ const TableTemplateDirectory = ({ directory, directoryWidth, onSelectItem, SKs =
     }
   };
 
-  // Determine the unique column keys from the first object
-  const columnKeys = directory.length > 0 ? Object.keys(directory[0]) : [];
-
   return (
     <div className="tableTemplate-directory" style={{ width: `${directoryWidth}px` }}>
       <table>
         <thead>
           <tr>
-            {columnKeys.map((key) => (
-              <th key={key}>{key}</th>
-            ))}
           </tr>
         </thead>
         <tbody>
-          {directory.map((item) => (
-            <tr key={item.SK || JSON.stringify(item)} onClick={() => handleItemClick(item)} style={{ cursor: 'pointer' }}>
-              {columnKeys.map((key) => (
-                <td key={key}>{item[key] || ''}</td>
-              ))}
+            <tr>
             </tr>
-          ))}
         </tbody>
       </table>
-
-      {/* <ul>
-        {directory.map((directoryitem) => (
-          <li key={directoryitem.SK}>
-            <a href="#" onClick={(e) => { e.preventDefault(); handleItemClick(directoryitem); }}>
-              {directoryitem.SK}
-            </a>
-          </li>
-        ))}
-      </ul> */}
     </div>
   );
 };
