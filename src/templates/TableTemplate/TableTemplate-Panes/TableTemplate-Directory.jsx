eda0ac79c423adb72799069ed950f3e7d8dccc74
Montology <solutions@montologyco.com>
2025-08-22 16:38:29 -0400

    Directory table

--- src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
+++ src/templates/TableTemplate/TableTemplate-Panes/TableTemplate-Directory.jsx
@@ -9,6 +9,26 @@ const TableTemplateDirectory = ({ directory, directoryWidth, onSelectItem, SKs =
 
   return (
     <div className="tableTemplate-directory" style={{ width: `${directoryWidth}px` }}>
+      <table>
+        <thead>
+          <tr>
+            {allKeys.map(key => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {directory.map((item) => (
+            <tr key={item.SK} onClick={() => handleItemClick(item)} style={{ cursor: 'pointer' }}>
+              {allKeys.map((key) => (
+                <td key={key}>{item[key] || ''}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>DELETE</h3>
       <ul>
         {directory.map((directoryitem) => (
           <li key={directoryitem.SK}>
@@ -18,6 +38,7 @@ const TableTemplateDirectory = ({ directory, directoryWidth, onSelectItem, SKs =
           </li>
         ))}
       </ul>
+      
     </div>
   );
 };
